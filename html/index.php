26a6bc03ccbc6b295202eb2c00f5b7f742bdbb66
PetrVorlicek <vorlicek.petricek@gmail.com>
2023-10-10 23:35:01 +0200

    Add better placeholder php

--- html/index.php
+++ html/index.php
@@ -1,23 +1,16 @@
 <html>
-
-<body>
-    $_REQUEST:
-    <?php #Print REQUEST data here ?>
-    <br>
-    $_GET:
-    <?php #Print GET data here ?>
-    <br>
-    $_POST:
-    <?php #Print POST data here ?>
-    <form method="get">
-        GET Form: <input type="text" name="get_name">
-        <input type="submit" value="Submit GET">
-    </form>
-    <form method="post">
-        POST Form: <input type="text" name="post_name">
-        <input type="submit" value="Submit POST">
-    </form>
-    <a href="index.php">Reset</a>
-</body>
-
+    <body>
+        <form method="post" action="secret.php">
+            Likáš:
+            <input type="text" name="name">
+            <br>
+            Jiránek:
+            <input type="text" name="surname">
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+        <br>
+        <p>?</p>
+        <a href="index.php">Reset</a>
+    </body>
 </html>
